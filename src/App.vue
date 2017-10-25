35a65fc64f9431800196b994f854dd55fdc65d12
machut <nmachut@ippon.fr>
2017-10-25 11:24:15 +0200

    import component in html

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,6 @@
 <template>
   <div id="app">
-    <nav class="navbar navbar-default">
-      <div class="container-fluid">
-        <div class="navbar-header">
-          <router-link class="navbar-brand" to="/"><span @click="toggle()">VueSandBox</span></router-link>
-        </div>
-        <ul class="nav navbar-nav">
-          <li @click="toggle()" :class="{active : actualPage === 'Album'}"><router-link to="/album">Album</router-link></li>
-          <li @click="toggle()" :class="{active : actualPage === 'Page2'}"><router-link to="/page2">Page2</router-link></li>
-          <li @click="toggle()" :class="{active : actualPage === 'Page3'}"><router-link to="/page3">Page3</router-link></li>
-          <li @click="toggle()" :class="{active : actualPage === 'Page4'}"><router-link to="/page4">Page4</router-link></li>
-        </ul>
-      </div>
-    </nav>
+    <MyHeader></MyHeader>
 
     <div class="container-fluid">
       <router-view/>
@@ -22,20 +10,11 @@
 </template>
 
 <script>
+import MyHeader from './components/Header.vue'
 export default {
   name: 'app',
-  data () {
-    return {
-      actualPage: ''
-    }
-  },
-  mounted: function () {
-    this.toggle();
-  },
-  methods: {
-    toggle: function () {
-      this.actualPage = this.$router.history.current.name;
-    }
+  components: {
+    MyHeader
   }
 }
 </script>
---
+++ src/components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <router-link class="navbar-brand" to="/"><span @click="toggle()">VueSandBox</span></router-link>
+      </div>
+      <ul class="nav navbar-nav">
+        <li @click="toggle()" :class="{active : actualPage === 'Album'}"><router-link to="/album">Album</router-link></li>
+        <li @click="toggle()" :class="{active : actualPage === 'Page2'}"><router-link to="/page2">Page2</router-link></li>
+        <li @click="toggle()" :class="{active : actualPage === 'Page3'}"><router-link to="/page3">Page3</router-link></li>
+        <li @click="toggle()" :class="{active : actualPage === 'Page4'}"><router-link to="/page4">Page4</router-link></li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+    export default {
+      name: 'MyHeader',
+      data () {
+        return {
+          actualPage: ''
+        }
+      },
+      mounted: function () {
+        this.toggle();
+      },
+      methods: {
+        toggle: function () {
+          this.actualPage = this.$router.history.current.name;
+        }
+      },
+    }
+</script>
+
+<style scoped>
+
+</style>
