ddd0539feda8657dbabc70553b4f2983fe8ad722
machut <nmachut@ippon.fr>
2017-10-25 11:07:46 +0200

    use transitions with routes

--- src/components/Page2.vue
+++ src/components/Page2.vue
@@ -5,7 +5,9 @@
       <li><router-link to="/page2/child1">child 1</router-link></li>
       <li><router-link to="/page2/child2">child 2</router-link></li>
     </ul>
-    <router-view></router-view>
+    <transition name="bounce">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
@@ -16,5 +18,21 @@
 </script>
 
 <style scoped>
-
+  .bounce-enter-active {
+    animation: bounce-in .5s;
+  }
+  .bounce-leave-active {
+    animation: bounce-in .5s reverse;
+  }
+  @keyframes bounce-in {
+    0% {
+      transform: scale(0);
+    }
+    50% {
+      transform: scale(1.5);
+    }
+    100% {
+      transform: scale(1);
+    }
+  }
 </style>
